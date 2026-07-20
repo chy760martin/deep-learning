eabc529df7ea5ba0e40a122ef655cb8b3ab01641
chy760martin <chy760martin@gmail.com>
2026-07-20 14:00:25 +0900

    calendar_scheduler 업데이트

--- Flutter_APP/calendar_scheduler/lib/main.dart
+++ Flutter_APP/calendar_scheduler/lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:calendar_scheduler/database/drift_database.dart';
 import 'package:flutter/material.dart';
 import 'package:calendar_scheduler/screen/home_screen.dart';
+import 'package:get_it/get_it.dart';
 import 'package:intl/date_symbol_data_local.dart';
 
 
@@ -9,6 +11,12 @@ void main() async {
 
   // intl 패키지 초기화(다국어화)
   await initializeDateFormatting();
+
+  // 데이터베이스 생성
+  final database = LocalDatabase();
+
+  // GetIt에 데이터베이스 변수 주입하기, 전역에서 사용
+  GetIt.I.registerSingleton<LocalDatabase>(database);
   
   runApp(
     MaterialApp(
